77d205abf026a04d540b1edcfd10331bfea04682
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-06 18:06:03 -0800

    Style hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 
+import '../layout/components/hero.sass';
+
 class Hero extends Component {
   render() {
     return (
-      <section class="hero">
-        <h1 class="hero__header">
+      <section className="hero">
+        <h1 className="hero__header">
           DEFI ACCELERATOR
         </h1>
-        <hr class="hero__hr" />
-        <p class="hero__info">
+        <hr className="hero__hr" />
+        <p className="hero__info">
           Earn up to 2x interest rate on compound by taking advantage of arbitrage 
           opportunities whilst automatically managing risk.
         </p>
-        <button class="hero__button">
-          Deposit
-        </button>
-        <button class="hero__button">
-          Withdrawal
-        </button>
+        <div className="hero__buttons">
+          <button className="hero__button">
+            Deposit
+          </button>
+          <button className="hero__button">
+            Withdrawal
+          </button>
+        </div>
       </section>
     );
   }
