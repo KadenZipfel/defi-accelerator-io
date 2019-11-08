78bfadaf5c01c743158715d0251dd9e2f982b274
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-07 20:10:58 -0800

    Add commasto numbers

--- src/components/CompareBox.js
+++ src/components/CompareBox.js
@@ -16,25 +16,30 @@ class CompareBox extends Component {
     }
   }
 
+  // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+  numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   render() {
     let compareBoxItems = [];
 
     const defiAcceleratorValues = [
       [['Interest Rate:'], [`%${(this.state.defiAcceleratorInterestRateMantissa * 100).toFixed(3)}`]],
-      [['Balance:'], [`${this.props.amount}`]],
-      [['Yearly Interest:'], [`${(this.props.amount * this.state.defiAcceleratorInterestRateMantissa).toFixed(2)}`]],
-      [['Monthly Interest:'], [`${((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 12).toFixed(2)}`]],
-      [['Weekly Interest:'], [`${((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 52).toFixed(2)}`]],
-      [['Daily Interest:'], [`${((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 365).toFixed(2)}`]]
+      [['Balance:'], [`$${this.numberWithCommas(this.props.amount)}`]],
+      [['Yearly Interest:'], [`$${this.numberWithCommas((this.props.amount * this.state.defiAcceleratorInterestRateMantissa).toFixed(2))}`]],
+      [['Monthly Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 12).toFixed(2))}`]],
+      [['Weekly Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 52).toFixed(2))}`]],
+      [['Daily Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.defiAcceleratorInterestRateMantissa) / 365).toFixed(2))}`]]
     ]
 
     const compoundValues = [
       [['Interest Rate:'], [`%${(this.state.compoundInterestRateMantissa * 100).toFixed(3)}`]],
-      [['Balance:'], [`${this.props.amount}`]],
-      [['Yearly Interest:'], [`${(this.props.amount * this.state.compoundInterestRateMantissa).toFixed(2)}`]],
-      [['Monthly Interest:'], [`${((this.props.amount * this.state.compoundInterestRateMantissa) / 12).toFixed(2)}`]],
-      [['Weekly Interest:'], [`${((this.props.amount * this.state.compoundInterestRateMantissa) / 52).toFixed(2)}`]],
-      [['Daily Interest:'], [`${((this.props.amount * this.state.compoundInterestRateMantissa) / 365).toFixed(2)}`]],
+      [['Balance:'], [`$${this.numberWithCommas(this.props.amount)}`]],
+      [['Yearly Interest:'], [`$${this.numberWithCommas((this.props.amount * this.state.compoundInterestRateMantissa).toFixed(2))}`]],
+      [['Monthly Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.compoundInterestRateMantissa) / 12).toFixed(2))}`]],
+      [['Weekly Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.compoundInterestRateMantissa) / 52).toFixed(2))}`]],
+      [['Daily Interest:'], [`$${this.numberWithCommas(((this.props.amount * this.state.compoundInterestRateMantissa) / 365).toFixed(2))}`]],
     ]
 
     if(this.props.header === "Defi Accelerator") {
