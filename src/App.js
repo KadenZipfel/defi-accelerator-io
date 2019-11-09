82b020ab620b2f12c12f4b27a032b111b8842e75
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-08 21:57:07 -0800

    Add scroll animations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,45 @@ import Footer from './components/Footer';
 import './layout/config/_base.sass';
 
 class App extends Component {
+  componentDidMount = () => {
+    this.triggerAnimation();
+  }
+
+  // Trigger an animation when the element is in the viewport
+  triggerAnimation = () => {
+    const compareHeading = document.querySelector('.compare__heading');
+    const compareInfo = document.querySelector('.compare__info');
+    const compareBoxes = document.querySelectorAll('.compare-box');
+    const howitworksHeading = document.querySelector('.how-it-works__heading');
+    const howitworksInfo = document.querySelector('.how-it-works__info');
+    const howitworksSections = document.querySelectorAll('.how-it-works__section');
+    const footerHeader = document.querySelector('.footer__header');
+    const footerForm = document.querySelector('.footer__form');
+    
+    const elements = [
+      compareHeading,
+      compareInfo,
+      compareBoxes[0],
+      compareBoxes[1],
+      howitworksHeading,
+      howitworksInfo,
+      howitworksSections[0],
+      howitworksSections[1],
+      howitworksSections[2],
+      howitworksSections[3],
+      footerHeader,
+      footerForm
+    ];
+
+    window.onscroll = () => {
+      elements.forEach((element) => {
+        if(window.scrollY + (window.innerHeight / 4) * 3 > element.offsetTop) {
+          element.classList.add('fadeup');
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <div>
