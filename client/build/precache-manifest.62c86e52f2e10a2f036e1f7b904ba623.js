ccc5183dcb5d6d9d2ab0459367ff4f7243539a7f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-12 15:23:08 -0800

    Set homepage

---
+++ client/build/precache-manifest.62c86e52f2e10a2f036e1f7b904ba623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4c35db99b0cef415f89ea50197632b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a022d386a997be836a",
+    "url": "/static/css/main.2f5b7418.chunk.css"
+  },
+  {
+    "revision": "cb3cf1e0077df892be56",
+    "url": "/static/js/2.970ee575.chunk.js"
+  },
+  {
+    "revision": "c5a022d386a997be836a",
+    "url": "/static/js/main.5248e737.chunk.js"
+  },
+  {
+    "revision": "d4d6a70235c1cf5d78e9",
+    "url": "/static/js/runtime-main.1c2589e8.js"
+  }
+]);
