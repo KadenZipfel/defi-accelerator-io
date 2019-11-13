48bc8dc2b4fc109399bc0b0c0c2bf2cb06c5d9f7
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-12 20:56:35 -0800

    Add whitepaper

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import '../layout/components/hero.sass';
 
+import whitepaper from '../documents/defi_accelerator.pdf';
+
 class Hero extends Component {
   render() {
     return (
@@ -14,9 +16,9 @@ class Hero extends Component {
           Earn up to 2x interest rate on compound by taking advantage of arbitrage 
           opportunities whilst automatically managing risk.
         </p>
-        <button className="hero__button">
+        <a className="hero__button" target="_blank" href={whitepaper}>
           Whitepaper
-        </button>
+        </a>
       </section>
     );
   }
